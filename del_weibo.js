66e432a709db231a78025dcd590182736fd7069a
xavier skip <xavierskip@gmail.com>
2012-07-25 13:23:57 +0800

    Update del_weibo.js
    tip to the end.

--- del_weibo.js
+++ del_weibo.js
@@ -6,17 +6,17 @@
 //   #在你的浏览器中按F12键打开控制台，复制粘贴以上代码在控制台中运行即可。
 //  （注：只支持chrome,firefox浏览器，其他的没有测试，IE的就算了，推荐使用chrome浏览器运行此脚本。）
 //
-//version:0.15
+//version:0.16
 // author:xavierskip
 
 function del_weibo(){
 	var array = [],
 		weibo_lists = document.getElementsByClassName("feed_list W_linecolor");
-        // get whole page weibo_mid
+    // get whole page weibo_mid
 	for(var i=0;i<weibo_lists.length;i++){
 		array.push(weibo_lists[i].getAttribute("mid"));
 	};
-	output('get all mid and ready to del!');      // output
+	output('get all mid and ready to del!');   // output
 	//sent XMLHttpRequest to del weibo 
 	function deletes(mid){
 		var	del   = new XMLHttpRequest();
@@ -29,18 +29,26 @@ function del_weibo(){
 		}else{
 			fail++;
 		};
-		output('delete'+mid+'weibo!');        // out put
+		output('delete weibo: '+mid);             // out put
 		return true;
 	};
+	//var start_time = new Date();
 	for(i in array){
 		deletes(array[i]);
-	};	
+	}
+	//var end_time = new Date();
+	//var time = (end_time - start_time)/1000;		
 };
 
 function next_page(){
 	var next_page = document.getElementsByClassName("W_pages")[0];
 	next_page.children[next_page.children.length-1].click();
 	next++;
+	// end 
+	if(count == next){
+			alert('删除掉'+done+'条微博！\n'+fail+'条删除失败');// out put	
+			location.reload();
+		};
 };
 
 function main(){
@@ -49,10 +57,10 @@ function main(){
 		//output("scrolling~");   
 		if(document.getElementsByClassName("W_pages")[0] != undefined){
     		clearInterval(onload_page);
-			output("loading page complete!!!!");     // out put
+			output("loading page complete!!!!"); // out put
 			del_weibo();
 			next_page();
-			output(next+" page done. jump to the next page");    // out put
+			output(next+" page done. jump to the next page");       // out put
     	}
     },1000);//可以适当调小一下这个数值。至于你的电脑会不会滚烂。。。我也不知道！
 };
@@ -62,13 +70,9 @@ function output(content){
 };
 
 function goto_del(number){
-	for (var x=0;x<number;x++){
-		if(x==number){
-			alert('删除掉'+done+'条微博！\n'+fail+'条删除失败');  // out put	
-			location.reload();
-		}else{
-			main();
-		};
+	count =number;   //Global
+	for (var x=0;x<=number;x++){
+		main();
 	};
 };
 
