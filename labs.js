5532c16819496da07f07337802ff8c76ac55fafa
xavier skip <xavierskip@gmail.com>
2012-08-29 21:35:46 +0800

    up
    up

--- labs.js
+++ labs.js
@@ -37,3 +37,31 @@ for (i in cids){
 //  http://weibo.com/*userID*/fans
 
 
+
+
+// API 
+
+var api = new XMLHttpRequest();
+api.open('get','https://api.weibo.com/2/friendships/followers.json?source=1528555305&uid=1779182613&count=200',false);
+api.send();
+var res = JSON.parse(api.response);
+for(var i=1;i<res.users.length;i++)
+	{
+		console.log(i+':'+res.users[i].name);
+	}
+console.log('totle:'+res.users.length)
+
+
+
+function api(method,url,data){
+	var api = new XMLHttpRequest();
+	api.open(method,url+'?'+data,false);
+	api.send();
+	return JSON.parse(api.response);
+};
+var response = api('get','https://api.weibo.com/2/comments/by_me.json','source=1528555305&count=200');
+var id = [];
+for(var i=0;i<response.comments.length;i++){
+	id.push(response.comments[i].id)
+}
+console.log(id)
