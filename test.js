490ba0c29e1ce9ce367ae4d413c673218e9571b7
xavier skip <xavierskip@gmail.com>
2015-08-10 21:26:06 +0800

    send weibo

---
+++ test.js
@@ -0,0 +1,88 @@
+// send random weibo
+function faweibo(content){
+    var oReq = new XMLHttpRequest();
+    oReq.open('post','http://weibo.com/aj/mblog/add', true);
+    oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    oReq.onreadystatechange = function(e){
+        if(oReq.readyState === 4){
+            var Return = eval("("+oReq.response+")");
+            if(Return.code=='100000'){
+                console.log("ok",content)
+            }else{
+                console.log("fail",content)
+            };
+        }
+    }
+    oReq.send('text='+content+'&_t=0');
+}
+for(var i=0;i<100;i++){
+    setTimeout(function(){
+        var c = Math.random().toString(36).substr(2);
+        faweibo(c)
+    },i*1200);//interval time
+}
+
+// http://weibo.com/comment/outbox
+function del_commit(cid){
+    var del = new XMLHttpRequest();
+    del.open('post','http://weibo.com/aj/comment/del?',true);
+    del.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    del.send('cid='+cid+'&_t=0');
+    console.log('del\t'+cid)
+    // end
+    count++
+    if (count == cids.length){
+        console.log('well done')
+    }   
+}
+
+var count = 0,
+    cids  = [],
+    hover = document.getElementsByClassName('hover')
+
+for (var i=0;i<hover.length;i++){
+    var a = hover[i].children[0]
+    var cid = a.getAttribute('action-data').slice(4)
+    cids.push(cid)
+}
+
+for (i in cids){
+    del_commit(cids[i])
+}
+
+//  http://weibo.com/*userID*/follow
+
+
+
+
+
+//  http://weibo.com/*userID*/fans
+
+
+
+
+// API
+var api = new XMLHttpRequest();
+api.open('get','https://api.weibo.com/2/friendships/followers.json?source=1528555305&uid=1779182613&count=200',false);
+api.send();
+var res = JSON.parse(api.response);
+for(var i=1;i<res.users.length;i++)
+    {
+        console.log(i+':'+res.users[i].name);
+    }
+console.log('totle:'+res.users.length)
+
+
+
+function api(method,url,data){
+    var api = new XMLHttpRequest();
+    api.open(method,url+'?'+data,false);
+    api.send();
+    return JSON.parse(api.response);
+};
+var response = api('get','https://api.weibo.com/2/comments/by_me.json','source=1528555305&count=200');
+var id = [];
+for(var i=0;i<response.comments.length;i++){
+    id.push(response.comments[i].id)
+}
+console.log(id)
